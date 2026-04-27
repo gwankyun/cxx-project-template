bb0e6fdf6714065e1d884f7aaefb2e1b9b4406bd
權少 <ljqican@163.com>
2026-04-28 07:02:48 +0800

    一、精簡複製dll腳本。 二、添加清單模式支持。

--- cmake/clang.cmake
+++ cmake/clang.cmake
@@ -1,20 +1,21 @@
 ﻿# 複製 `path` 路徑下的 `libc++.dll` 和 `libunwind.dll` 到 `target` 生成文件同目錄
-function(clang_copy_dll)
+function(target_copy_files)
   cmake_parse_arguments(
-    copy_dll_arg
-    ""
-    "PATH;TARGET"
-    "FILES"
+    copy_dll_arg # 前綴
+    "" # 選項值
+    # "PATH;TARGET" # 單值
+    "TARGET" # 單值
+    "FILES" # 多值
     ${ARGN}
   )
 
-  set(path ${copy_dll_arg_PATH})
+  # set(path ${copy_dll_arg_PATH})
   set(target ${copy_dll_arg_TARGET})
+  set(files ${copy_dll_arg_FILES})
 
-  # 查找 libc++.dll 和 libunwind.dll
-  set(bin_path "${path}")
-
-  set(src_list ${copy_dll_arg_FILES})
+  # message(STATUS "path: ${path}")
+  message(STATUS "target: ${target}")
+  message(STATUS "files: ${files}")
 
   # gersemi: off
   add_custom_command(
@@ -22,9 +23,10 @@ function(clang_copy_dll)
     POST_BUILD
     COMMAND
       ${CMAKE_COMMAND}
-      -D dll_list="${src_list}"
-      -D dest_path=""$<TARGET_FILE_DIR:${target}>""
-      -D bin_path="${bin_path}"
+      # -D dll_list=${files}
+      -D src_files=[==[${files}]==]
+      -D dest_dir=""$<TARGET_FILE_DIR:${target}>""
+      # -D source="${path}"
       -P "${CMAKE_SOURCE_DIR}/cmake/copy_dll.cmake"
     COMMENT ""
   )
--- cmake/copy_dll.cmake
+++ cmake/copy_dll.cmake
@@ -1,16 +1,24 @@
-﻿message(STATUS "dll_list: ${dll_list}")
-message(STATUS "dest_path: ${dest_path}")
-message(STATUS "bin_path: ${bin_path}")
+﻿# message(STATUS "dll_list: ${dll_list}")
+# message(STATUS "dest_path: ${dest_path}")
+# message(STATUS "bin_path: ${bin_path}")
 
-string(REGEX MATCHALL "[^ ]+" list_res "${dll_list}")
+# string(REGEX MATCHALL "[^ ]+" list_res "${dll_list}")
+string(REGEX REPLACE "^\\[==\\[(.*)\\]==\\]$" "\\1" src_files "${src_files}")
 
-foreach(f IN LISTS list_res)
+foreach(f IN LISTS src_files)
   message(STATUS "f: ${f}")
 
-  set(libcpp_dll "${bin_path}")
-  cmake_path(APPEND libcpp_dll ${f})
+  cmake_path(GET f FILENAME app_name)
+  # cmake_print_variables(app_n ame2)
+  # message(STATUS "app_name2: ${app_name2}")
 
-  file(COPY_FILE ${libcpp_dll} ${dest_path}/${f} RESULT result ONLY_IF_DIFFERENT)
+  cmake_path(SET dest NORMALIZE "${dest_dir}/${app_name}")
+
+#   set(libcpp_dll "${bin_path}")
+#   cmake_path(APPEND libcpp_dll ${f})
+
+#   file(COPY_FILE ${libcpp_dll} ${dest_path}/${f} RESULT result ONLY_IF_DIFFERENT)
+  file(COPY_FILE ${f} ${dest} RESULT result ONLY_IF_DIFFERENT)
 
 endforeach()
 
