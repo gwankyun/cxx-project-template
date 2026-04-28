a23163ea552091180109cf7bece5a03a4c8ccfa6
權少 <ljqican@163.com>
2026-04-28 21:21:13 +0800

    一、修改一些CMake腳本名。 二、適配VS。

---
+++ cmake/target_copy_files.cmake
@@ -0,0 +1,28 @@
+﻿function(target_copy_files)
+  cmake_parse_arguments(
+    copy_dll_arg # 前綴
+    "" # 選項值
+    "TARGET" # 單值
+    "FILES" # 多值
+    ${ARGN}
+  )
+
+  set(target ${copy_dll_arg_TARGET})
+  set(files ${copy_dll_arg_FILES})
+
+  message(DEBUG "target: ${target}")
+  message(DEBUG "files: ${files}")
+
+  # gersemi: off
+  add_custom_command(
+    TARGET ${target}
+    POST_BUILD
+    COMMAND
+      ${CMAKE_COMMAND}
+      -D src_files=[==[${files}]==]
+      -D dest_dir="$<TARGET_FILE_DIR:${target}>"
+      -P "${CMAKE_SOURCE_DIR}/cmake/target_copy_files_impl.cmake"
+    COMMENT ""
+  )
+  # gersemi: on
+endfunction()
---
+++ cmake/target_copy_files_impl.cmake
@@ -0,0 +1,16 @@
+﻿message(DEBUG "src_files: ${src_files}")
+message(DEBUG "dest_dir: ${dest_dir}")
+
+# 去除前後的[==和]==
+string(REGEX REPLACE "^\\[==\\[(.*)\\]==\\]$" "\\1" src_files "${src_files}")
+
+foreach(f IN LISTS src_files)
+  message(DEBUG "f: ${f}")
+
+  cmake_path(GET f FILENAME app_name)
+
+  cmake_path(SET dest NORMALIZE "${dest_dir}/${app_name}")
+
+  file(COPY_FILE ${f} ${dest} RESULT result ONLY_IF_DIFFERENT)
+
+endforeach()
