e9f70de03fc65db153c7467b5300d2560959d05e
權少 <ljqican@163.com>
2026-05-09 21:14:45 +0800

    整理代碼。

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 ﻿#include <fstream>
 
+#include <boost/scope/defer.hpp>
 #include <nlohmann/json.hpp>
+#include <quill/Backend.h>
 #include <quill/LogFunctions.h>
 #include <quill/SimpleSetup.h>
+#include <quill/sinks/ConsoleSink.h>
 
 #include "common.hpp"
 #include "compiler.h"
@@ -11,12 +14,24 @@ using json = nlohmann::json;
 
 int main(int _argc, char* _argv[])
 {
-    auto* logger = quill::simple_logger();
+    // auto logger = quill::simple_logger();
+
+    quill::Backend::start();
+    BOOST_SCOPE_DEFER[&]
+    {
+        quill::Backend::stop();
+    };
+
+    auto console_sink = quill::Frontend::create_or_get_sink<quill::ConsoleSink>("sink_id_1");
+    auto* logger = quill::Frontend::create_or_get_logger(
+        "console", console_sink,
+        quill::PatternFormatterOptions{"[%(time)] [%(caller_function:<8):%(line_number:>4)] %(message)",
+                                       "%Y/%M/%d %H:%M:%S:%Qms", quill::Timezone::GmtTime});
 
     auto exe_path = getExecutablePath();
     quill::info(logger, "exe_path: {}", exe_path.string());
 
-    int i = 9;
+    int i{9};
 
     quill::info(logger, "compiler id     : {}", compiler::id());
     quill::info(logger, "compiler version: {}", compiler::version());
--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,6 @@
 ﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <fstream>
+#include <vector>
 
 #include <boost/scope/defer.hpp>
 #include <doctest/doctest.h>
@@ -7,7 +8,6 @@
 
 #include "common.hpp"
 #include "compiler.h"
-
 
 using json = nlohmann::json;
 
