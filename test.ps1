83ac75e238995291aeb77dac2f7cd59af7d97114
權少 <ljqican@163.com>
2026-05-15 07:33:13 +0800

    添加工作流預設。

--- test.ps1
+++ test.ps1
@@ -2,23 +2,4 @@ param(
     [Parameter(Mandatory=$true)][ValidateSet('msvc','gcc','clang','clangcl')][string]$compiler
 )
 
-cmake --preset $compiler
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "error: cmake --preset $compiler"
-    exit 1
-}
-
-cmake --build --preset $compiler
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "cmake --build --preset $compiler"
-    exit 1
-}
-
-ctest --preset $compiler
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "ctest --preset $compiler"
-    exit 1
-}
+cmake --workflow --preset $compiler
